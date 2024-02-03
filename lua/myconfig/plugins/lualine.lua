3b88aa1dec68a1b4b54e128e796d732a5e91ca95
remvn <34063162+remvn@users.noreply.github.com>
2024-02-04 03:56:14 +0700

    custom lualine theme

--- lua/myconfig/plugins/lualine.lua
+++ lua/myconfig/plugins/lualine.lua
@@ -2,43 +2,22 @@ return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
-        local colors = {
-            bg = "#353536",
-            file_name = "#ced1d4",
-            mode = "#ced1d4",
-            branch = "#ced1d4",
-            text = "#ced1d4",
-            file_type = "#ced1d4",
-            location = "#ced1d4",
-            progress = "#ced1d4",
-        }
-        local theme = {
-            normal = {
-                a = { fg = colors.mode, bg = colors.bg },
-                b = { fg = colors.branch, bg = colors.bg },
-                c = { fg = colors.file_name, bg = colors.bg },
-                x = { fg = colors.file_type, bg = colors.bg },
-                y = { fg = colors.progress, bg = colors.bg },
-                z = { fg = colors.location, bg = colors.bg },
-            },
-        }
-
         require('lualine').setup({
             options = {
-                theme = 'auto',
+                theme = 'darcula-custom-tmp',
                 component_separators = '',
                 section_separators = { left = '', right = '' },
             },
             sections = {
-                lualine_a = {},
-                lualine_b = {},
-                lualine_c = {
+                lualine_a = {
                     {
                         'filename',
                         path = 1,
                         file_status = true,
                     }
                 },
+                lualine_b = {},
+                lualine_c = {},
                 lualine_x = {
                     {
                         'diagnostics',
