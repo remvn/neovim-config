522b15faa2e76baa50f9ec7ce4db1781b35f8efb
remvn <34063162+remvn@users.noreply.github.com>
2024-03-11 22:45:26 +0700

    nvim substitute

---
+++ lua/myconfig/plugins/substitute.lua
@@ -0,0 +1,19 @@
+return {
+    {
+        "gbprod/substitute.nvim",
+        config = function()
+            local substitute = require("substitute")
+            substitute.setup({
+                highlight_substituted_text = {
+                    enabled = true,
+                    timer = 500,
+                },
+            })
+
+            vim.keymap.set("n", "s", substitute.operator, { noremap = true })
+            vim.keymap.set("n", "ss", substitute.line, { noremap = true })
+            vim.keymap.set("x", "s", substitute.visual, { noremap = true })
+            -- vim.keymap.set("n", "S", substitute.eol, { noremap = true })
+        end
+    },
+}
