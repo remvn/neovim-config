09a216665c4f21eb5d29bb6dea36f56a657ee40e
remvn <34063162+remvn@users.noreply.github.com>
2024-03-16 21:20:01 +0700

    refactor lualine

--- lua/myconfig/plugins/lualine.lua
+++ lua/myconfig/plugins/lualine.lua
@@ -7,30 +7,30 @@ local function trunc(trunc_width, trunc_len, hide_width, no_ellipsis)
     return function(str)
         local win_width = vim.fn.winwidth(0)
         if hide_width and win_width < hide_width then
-            return ''
+            return ""
         elseif trunc_width and trunc_len and win_width < trunc_width and #str > trunc_len then
-            return str:sub(1, trunc_len) .. (no_ellipsis and '' or '...')
+            return str:sub(1, trunc_len) .. (no_ellipsis and "" or "...")
         end
         return str
     end
 end
 
 return {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
         local colors = {
-            bg        = "#3a3a3b",
-            fade      = "#898b8d",
+            bg = "#3a3a3b",
+            fade = "#898b8d",
             editor_bg = "#303031",
-            text      = "#d6d9dc",
+            text = "#d6d9dc",
             -- white     = "#e2e5e9",
-            white     = "#ffffff",
-            black     = '#000000',
-            green     = '#9db871',
-            orange    = '#e18437',
-            gray      = '#adadb1',
-            yellow    = '#ffcf6b'
+            white = "#ffffff",
+            black = "#000000",
+            green = "#9db871",
+            orange = "#e18437",
+            gray = "#adadb1",
+            yellow = "#ffcf6b",
         }
 
         local default = { fg = colors.text, bg = colors.bg }
@@ -45,78 +45,88 @@ return {
             },
             insert = {
                 y = { fg = colors.green, bg = colors.editor_bg },
-                z = { fg = colors.black, bg = colors.green, },
+                z = { fg = colors.black, bg = colors.green },
             },
             visual = {
                 y = { fg = colors.orange, bg = colors.editor_bg },
-                z = { fg = colors.black, bg = colors.orange, },
+                z = { fg = colors.black, bg = colors.orange },
             },
             command = {
                 y = { fg = colors.white, bg = colors.editor_bg },
-                z = { fg = colors.black, bg = colors.white, },
+                z = { fg = colors.black, bg = colors.white },
             },
         }
 
-        require('lualine').setup({
+        local tabs = {
+            "tabs",
+            mode = 3,
+            tabs_color = {
+                active = "lualine_z_insert", -- Color for active tab.
+                inactive = "lualine_a_inactive", -- Color for inactive tab.
+            },
+            symbols = {
+                modified = " [+]", -- Text to show when the file is modified.
+            },
+        }
+
+        local filename = {
+            "filename",
+            path = 1,
+            file_status = true,
+        }
+
+        local diagnostics = {
+            "diagnostics",
+            sources = { "nvim_diagnostic" },
+            -- symbols = { error = " ", warn = " ", hint = " ", info = " ", },
+            symbols = { error = "E:", warn = "W:", info = "I:", hint = "H:" },
+        }
+
+        local diff = { "diff" }
+        local branch = { "branch", icon = "" }
+        local filetype = { "filetype" }
+
+        local lsp_status = {
+            "lspstatus",
+            icon = {
+                "󰌵",
+                color = { fg = colors.yellow },
+            },
+            fmt = trunc(120, 4, 60, false),
+        }
+
+        local progress = { "progress" }
+        local location = { "location" }
+
+        require("lualine").setup({
             options = {
                 theme = theme,
-                section_separators = { left = '', right = '' },
-                component_separators = { left = '', right = '' },
+                section_separators = { left = "", right = "" },
+                component_separators = { left = "", right = "" },
             },
             tabline = {
                 lualine_a = {
-                    {
-                        "tabs",
-                        mode = 3,
-                        tabs_color = {
-                            active = 'lualine_z_insert',     -- Color for active tab.
-                            inactive = 'lualine_a_inactive', -- Color for inactive tab.
-                        },
-                        symbols = {
-                            modified = ' [+]', -- Text to show when the file is modified.
-                        },
-                    },
-                }
+                    tabs,
+                },
             },
             sections = {
                 lualine_a = {
-                    {
-                        'filename',
-                        path = 1,
-                        file_status = true,
-                    },
-                    {
-                        'diagnostics',
-                        sources = { 'nvim_diagnostic' },
-                        -- symbols = {
-                        --     error = " ",
-                        --     warn = " ",
-                        --     hint = " ",
-                        --     info = " ",
-                        -- },
-                        symbols = { error = 'E:', warn = 'W:', info = 'I:', hint = 'H:' },
-                    },
+                    filename,
+                    diagnostics,
                 },
                 lualine_b = {},
                 lualine_c = {},
                 lualine_x = {
-                    { 'diff' },
-                    { 'branch', icon = '' },
-                    'filetype',
-                    {
-                        'lspstatus',
-                        icon = {
-                            '󰌵',
-                            color = { fg = colors.yellow },
-                        },
-                        fmt = trunc(120, 4, 60, false)
-                    },
+                    diff,
+                    branch,
+                    filetype,
+                    lsp_status,
                 },
-                lualine_y = { 'progress' },
-                lualine_z = { 'location' }
-            }
+                lualine_y = { progress },
+                lualine_z = { location },
+            },
         })
 
         vim.opt.showtabline = 1
-    end
+    end,
 }
