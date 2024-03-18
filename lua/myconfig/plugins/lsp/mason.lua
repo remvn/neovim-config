e272cd178c33eb6cca0858e48bd40cb624f24ee4
remvn <34063162+remvn@users.noreply.github.com>
2024-03-19 01:13:36 +0700

    vue support

--- lua/myconfig/plugins/lsp/mason.lua
+++ lua/myconfig/plugins/lsp/mason.lua
@@ -9,38 +9,42 @@ return {
     config = function()
         local lsp_zero = require("lsp-zero")
         local lspconfig = require("lspconfig")
+        local mason = require("mason")
         local mason_registry = require("mason-registry")
-        require("mason").setup()
+        local mason_lsp = require("mason-lspconfig")
 
-        require("mason-lspconfig").setup({
+        mason.setup()
+        mason_lsp.setup({
             ensure_installed = {
                 "tsserver",
+                "volar",
                 "lua_ls",
                 "gopls",
                 "golangci_lint_ls",
-                "volar",
                 "bashls",
             },
             automatic_installation = true,
-            handlers = {
-                -- lsp-zero already handle these config below:
-                -- * cmp-nvim-lsp capabilities
-                -- * on_attach func
-                -- see: lua/myconfig/plugins/lsp/lsp-config.lua
-                -- therefore default_setup only call empty setup
-                -- under the hood. (I guess)
-                lsp_zero.default_setup,
-            },
         })
 
-        -- config lua_ls for neovim
-        local lua_opts = lsp_zero.nvim_lua_ls()
-        lspconfig.lua_ls.setup(lua_opts)
+        mason_lsp.setup_handlers({
+            -- lsp-zero already handle these config below:
+            -- * cmp-nvim-lsp capabilities
+            -- * on_attach func
+            -- see: lua/myconfig/plugins/lsp/lsp-config.lua
+            -- therefore default_setup only call empty setup
+            -- under the hood. (I guess)
+            lsp_zero.default_setup,
+            lua_ls = function()
+                -- config lua_ls for neovim
+                local lua_opts = lsp_zero.nvim_lua_ls()
+                lspconfig.lua_ls.setup(lua_opts)
+            end,
+        })
 
         -- vue support through tsserver plugin
         local has_volar, volar = pcall(mason_registry.get_package, "vue-language-server")
         if has_volar then
-            local filetypes = {
+            local vueFiletypes = {
                 "typescript",
                 "javascript",
                 "vue",
@@ -50,11 +54,18 @@ return {
             local vue_plugin = {
                 name = "@vue/typescript-plugin",
                 location = vue_ts_plugin_path,
-                languages = filetypes,
+                languages = vueFiletypes,
             }
 
-            local tsserverFiletypes = lspconfig.tsserver.filetypes
-            vim.list_extend(tsserverFiletypes, { "vue" })
+            local tsserverFiletypes = {
+                "javascript",
+                "javascriptreact",
+                "javascript.jsx",
+                "typescript",
+                "typescriptreact",
+                "typescript.tsx",
+                "vue",
+            }
             lspconfig.tsserver.setup({
                 filetypes = tsserverFiletypes,
                 init_options = {
@@ -63,8 +74,6 @@ return {
                     },
                 },
             })
-
-            -- print(vim.inspect(lspconfig.tsserver.manager.config.init_options))
         end
     end,
 }
