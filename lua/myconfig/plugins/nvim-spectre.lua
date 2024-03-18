4965ce72167332ac402c6adbf00d6eaa10ed6221
remvn <34063162+remvn@users.noreply.github.com>
2024-03-18 17:18:07 +0700

    fix spectre visual lag

--- lua/myconfig/plugins/nvim-spectre.lua
+++ lua/myconfig/plugins/nvim-spectre.lua
@@ -4,31 +4,17 @@ return {
         "nvim-lua/plenary.nvim",
     },
     config = function()
-        local spectre = require("spectre")
-        spectre.setup()
-
-        vim.keymap.set("n", "<leader>S", function()
-            spectre.toggle()
-        end, {
-            desc = "spectre: toggle",
+        vim.keymap.set("n", "<leader>S", '<cmd>lua require("spectre").toggle()<CR>', {
+            desc = "Spectre: toggle",
         })
-
-        vim.keymap.set("n", "<leader>sw", function()
-            spectre.open_visual({ select_word = true })
-        end, {
-            desc = "spectre: search current word",
+        vim.keymap.set("n", "<leader>sw", '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', {
+            desc = "Spectre: search current word",
         })
-
-        vim.keymap.set("v", "<leader>sw", function()
-            spectre.open_visual()
-        end, {
-            desc = "spectre: search current word",
+        vim.keymap.set("v", "<leader>sw", '<esc><cmd>lua require("spectre").open_visual()<CR>', {
+            desc = "Spectre: search current word",
         })
-
-        vim.keymap.set("n", "<leader>sp", function()
-            spectre.open_file_search({ select_word = true })
-        end, {
-            desc = "spectre: search on current file",
+        vim.keymap.set("n", "<leader>sp", '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', {
+            desc = "Spectre: search on current file",
         })
     end,
 }
