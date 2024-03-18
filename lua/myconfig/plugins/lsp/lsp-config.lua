b538cf8e2dc83369dad672dcddfe6b70b3a58a74
remvn <34063162+remvn@users.noreply.github.com>
2024-03-19 01:20:35 +0700

    refactor

--- lua/myconfig/plugins/lsp/lsp-config.lua
+++ lua/myconfig/plugins/lsp/lsp-config.lua
@@ -70,50 +70,17 @@ return {
             lsp_zero.on_attach(function(client, bufnr)
                 local opts = { buffer = bufnr, remap = false }
 
-                vim.keymap.set("n", "gd", function()
-                    vim.lsp.buf.definition()
-                end, opts)
-
-                vim.keymap.set("n", "gh", function()
-                    vim.lsp.buf.hover()
-                end, opts)
-
-                vim.keymap.set("n", "gH", function()
-                    vim.lsp.buf.references()
-                end, opts)
-
-                vim.keymap.set("n", "gi", function()
-                    vim.lsp.buf.implementation()
-                end, opts)
-
-                vim.keymap.set("n", "<leader>ws", function()
-                    ---@diagnostic disable-next-line: missing-parameter
-                    vim.lsp.buf.workspace_symbol()
-                end, opts)
-
-                vim.keymap.set("n", "<leader>ca", function()
-                    vim.lsp.buf.code_action()
-                end, opts)
-
-                vim.keymap.set("n", "<leader>r", function()
-                    vim.lsp.buf.rename()
-                end, opts)
-
-                vim.keymap.set("n", "<leader>of", function()
-                    vim.diagnostic.open_float()
-                end, opts)
-
-                vim.keymap.set("n", "[d", function()
-                    vim.diagnostic.goto_next()
-                end, opts)
-
-                vim.keymap.set("n", "]d", function()
-                    vim.diagnostic.goto_prev()
-                end, opts)
-
-                vim.keymap.set("i", "<C-k>", function()
-                    vim.lsp.buf.signature_help()
-                end, opts)
+                vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+                vim.keymap.set("n", "gh", vim.lsp.buf.hover, opts)
+                vim.keymap.set("n", "gH", vim.lsp.buf.references, opts)
+                vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+                vim.keymap.set("n", "<leader>ws", vim.lsp.buf.workspace_symbol, opts)
+                vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+                vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, opts)
+                vim.keymap.set("n", "<leader>of", vim.diagnostic.open_float, opts)
+                vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
+                vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
+                vim.keymap.set("i", "<C-k>", vim.lsp.buf.signature_help, opts)
             end)
         end,
     },
