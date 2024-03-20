f8818425ebba7337f64db6231351a8baaca3eadb
remvn <34063162+remvn@users.noreply.github.com>
2024-03-20 22:20:23 +0700

    update harpoon keymaps

--- lua/myconfig/plugins/harpoon.lua
+++ lua/myconfig/plugins/harpoon.lua
@@ -4,7 +4,7 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
         local harpoon = require("harpoon")
-        harpoon:setup()
+        harpoon:setup({})
         harpoon:extend({
             UI_CREATE = function(cx)
                 vim.keymap.set("n", "<leader>vs", function()
@@ -18,24 +18,39 @@ return {
                 vim.keymap.set("n", "<C-c>", function()
                     harpoon.ui:close_menu()
                 end, { buffer = cx.bufnr })
+
+                vim.opt.number = true
+                vim.opt.relativenumber = true
             end,
         })
 
-        vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
         local window_opts = {
             border = "rounded",
             title = " Harpoon ",
             ui_max_width = 70,
         }
 
-        vim.keymap.set("n", "<A-1>", function() harpoon.ui:toggle_quick_menu(harpoon:list(), window_opts) end)
-        vim.keymap.set("n", "<A-q>", function() harpoon:list():select(1) end)
-        vim.keymap.set("n", "<A-w>", function() harpoon:list():select(2) end)
-        vim.keymap.set("n", "<A-a>", function() harpoon:list():select(3) end)
-        vim.keymap.set("n", "<A-s>", function() harpoon:list():select(4) end)
+        vim.keymap.set("n", "<leader>a", function()
+            harpoon:list():append()
+        end)
+        vim.keymap.set("n", "<leader>ls", function()
+            harpoon.ui:toggle_quick_menu(harpoon:list(), window_opts)
+        end)
+        vim.keymap.set("n", "<A-q>", function()
+            harpoon:list():select(1)
+        end)
+        vim.keymap.set("n", "<A-w>", function()
+            harpoon:list():select(2)
+        end)
+        vim.keymap.set("n", "<A-a>", function()
+            harpoon:list():select(3)
+        end)
+        vim.keymap.set("n", "<A-s>", function()
+            harpoon:list():select(4)
+        end)
 
         -- Toggle previous & next buffers stored within Harpoon list
         -- vim.keymap.set("n", "<A-p>", function() harpoon:list():prev() end)
         -- vim.keymap.set("n", "<A-n>", function() harpoon:list():next() end)
-    end
+    end,
 }
--- lua/myconfig/plugins/lsp-signature.lua
+++ lua/myconfig/plugins/lsp-signature.lua
@@ -2,15 +2,15 @@ return {
     "ray-x/lsp_signature.nvim",
     event = "VeryLazy",
     config = function()
-        require "lsp_signature".setup({
+        require("lsp_signature").setup({
             bind = true,
             handler_opts = {
-                border = "rounded"
+                border = "rounded",
             },
-            hint_enable = false
+            hint_enable = false,
         })
-        vim.keymap.set("n", "<leader>ls", function()
-            require('lsp_signature').toggle_float_win()
-        end)
+        -- vim.keymap.set("n", "<leader>ls", function()
+        --     require('lsp_signature').toggle_float_win()
+        -- end)
     end,
 }
