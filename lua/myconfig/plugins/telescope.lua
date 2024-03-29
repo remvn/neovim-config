5f17978e22fde9e2c783707a59e0b40ad3c05234
remvn <34063162+remvn@users.noreply.github.com>
2024-03-30 03:29:00 +0700

    find files include hidden files and ignore .git

--- lua/myconfig/plugins/telescope.lua
+++ lua/myconfig/plugins/telescope.lua
@@ -10,8 +10,8 @@ return {
     },
     init = function()
         local builtin = require("telescope.builtin")
-        vim.keymap.set("n", "<C-p>", builtin.git_files, {})
-        vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
+        vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+        vim.keymap.set("n", "<leader>pf", builtin.git_files, {})
         vim.keymap.set("n", "<leader>pb", builtin.current_buffer_fuzzy_find, {})
         vim.keymap.set("n", "<leader>ds", builtin.lsp_document_symbols, {})
         vim.keymap.set("n", "<leader>vh", builtin.help_tags, {})
@@ -20,31 +20,39 @@ return {
         vim.keymap.set("n", "<leader>ps", function()
             builtin.grep_string({ search = vim.fn.input("Grep > ") })
         end)
+        vim.keymap.set("n", "<leader>pw", builtin.grep_string, {})
     end,
     config = function()
         local telescope = require("telescope")
         local actions = require("telescope.actions")
+        local telescopeConfig = require("telescope.config")
+
+        -- Clone the default Telescope configuration
+        local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
+        table.insert(vimgrep_arguments, "--hidden")
+        table.insert(vimgrep_arguments, "--glob")
+        table.insert(vimgrep_arguments, "!**/.git/*")
 
         telescope.setup({
             defaults = {
+                vimgrep_arguments = vimgrep_arguments,
                 mappings = {
                     i = {
                         -- actions.which_key shows the mappings for your picker,
                         -- e.g. git_{create, delete, ...}_branch for the git_branches picker
-                        ["<C-/>"] = "which_key",
+                        ["<C-/>"] = actions.which_key,
                         ["<C-j>"] = actions.move_selection_next,
                         ["<C-k>"] = actions.move_selection_previous,
+                        ["<C-h>"] = actions.file_split,
                         ["<M-q>"] = false,
-                    },
-                    n = {
-                        -- split window, should use another binding
-                        -- use this until something else is found
-                        ["<leader>vs"] = actions.file_vsplit,
-                        ["<leader>hs"] = actions.file_split,
                     },
                 },
             },
-            pickers = {},
+            pickers = {
+                find_files = {
+                    find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+                },
+            },
             extensions = {},
         })
 
