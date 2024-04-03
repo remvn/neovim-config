1543f73c9600e499d604f648c0712543419190cf
remvn <34063162+remvn@users.noreply.github.com>
2024-04-04 03:43:42 +0700

    test

--- lua/myconfig/plugins/comment.lua
+++ lua/myconfig/plugins/comment.lua
@@ -4,7 +4,7 @@ return {
         dependencies = { "nvim-lua/plenary.nvim" },
         opts = {
             signs = false,
-        }
+        },
     },
     -- {
     --     'tpope/vim-commentary',
@@ -20,40 +20,38 @@ return {
     --     end
     -- },
     {
-        'numToStr/Comment.nvim',
+        "numToStr/Comment.nvim",
         dependencies = {
-            'JoosepAlviste/nvim-ts-context-commentstring',
+            "JoosepAlviste/nvim-ts-context-commentstring",
         },
         lazy = false,
         config = function()
             vim.g.skip_ts_context_commentstring_module = true
-            require('ts_context_commentstring').setup {
+            require("ts_context_commentstring").setup {
                 enable_autocmd = false,
             }
-            require('Comment').setup {
+            require("Comment").setup {
                 mappings = {
                     basic = false,
                     extra = false,
                 },
-                pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+                pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
             }
-            local api = require('Comment.api')
+            local api = require("Comment.api")
 
-            vim.keymap.set('n', '<C-_>', api.toggle.linewise.current)
-            vim.keymap.set('n', '<C-\\>', api.toggle.blockwise.current)
+            vim.keymap.set("n", "<C-_>", api.toggle.linewise.current)
+            vim.keymap.set("n", "<C-\\>", api.toggle.blockwise.current)
 
-            local esc = vim.api.nvim_replace_termcodes(
-                '<Esc>', true, false, true
-            )
+            local esc = vim.api.nvim_replace_termcodes("<Esc>", true, false, true)
 
-            vim.keymap.set('x', '<C-_>', function()
-                vim.api.nvim_feedkeys(esc, 'nx', false)
+            vim.keymap.set("x", "<C-_>", function()
+                vim.api.nvim_feedkeys(esc, "nx", false)
                 api.toggle.linewise(vim.fn.visualmode())
             end)
-            vim.keymap.set('x', '<C-\\>', function()
-                vim.api.nvim_feedkeys(esc, 'nx', false)
+            vim.keymap.set("x", "<C-\\>", function()
+                vim.api.nvim_feedkeys(esc, "nx", false)
                 api.toggle.blockwise(vim.fn.visualmode())
             end)
-        end
-    }
+        end,
+    },
 }
