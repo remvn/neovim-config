f1c9a7b73ca675a479a0b8af67b3969376062520
remvn <34063162+remvn@users.noreply.github.com>
2024-05-13 17:21:24 +0700

    bqf dont preview fugitive buffer

--- lua/myconfig/plugins/nvim-bqf.lua
+++ lua/myconfig/plugins/nvim-bqf.lua
@@ -1,5 +1,5 @@
 ---@diagnostic disable: missing-fields
-return {
+local plugin = {
     "kevinhwang91/nvim-bqf",
     ft = "qf",
     config = function()
@@ -8,6 +8,23 @@ return {
                 pscrollup = "<C-u>",
                 pscrolldown = "<C-d>",
             },
+            preview = {
+                should_preview_cb = function(bufnr, qwinid)
+                    local ret = true
+                    local bufname = vim.api.nvim_buf_get_name(bufnr)
+                    local fsize = vim.fn.getfsize(bufname)
+                    if fsize > 100 * 1024 then
+                        -- skip file size greater than 100k
+                        ret = false
+                    elseif bufname:match("^fugitive://") then
+                        -- skip fugitive buffer
+                        ret = false
+                    end
+                    return ret
+                end,
+            },
         })
     end,
 }
+
+return plugin
