e5db31961c31155cd8447a8c16e65ea998d74db8
remvn <34063162+remvn@users.noreply.github.com>
2024-06-19 17:05:39 +0700

    fix open browser for markdown preview

--- lua/myconfig/plugins/markdown-preview.lua
+++ lua/myconfig/plugins/markdown-preview.lua
@@ -6,4 +6,24 @@ return {
     build = function()
         vim.fn["mkdp#util#install"]()
     end,
+    config = function()
+        if vim.fn.has("wsl") then
+            vim.api.nvim_exec2(
+                [[
+                function! MdpOpenPreview(url) abort
+                    let l:mdp_browser = '/mnt/c/Program\ Files/Google/Chrome/Application/chrome.exe'
+                    let l:mdp_browser_opts = '--new-tab'
+                    if !filereadable(substitute(l:mdp_browser, '\\ ', ' ', 'g'))
+                        let l:mdp_browser = '/mnt/c/Program\ Files\ \(x86\)/Microsoft/Edge/Application/msedge.exe'
+                        let l:mdp_browser_opts = '--new-tab'
+                    endif
+                    execute join(['silent! !', l:mdp_browser, l:mdp_browser_opts, a:url])
+                    redraw!
+                endfunction
+                ]],
+                { output = false }
+            )
+            vim.g.mkdp_browserfunc = "MdpOpenPreview"
+        end
+    end,
 }
