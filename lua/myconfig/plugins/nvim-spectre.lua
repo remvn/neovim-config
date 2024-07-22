446bb9ea879509cfeaf1cf640f3157f41167811d
remvn <34063162+remvn@users.noreply.github.com>
2024-07-23 05:01:42 +0700

    wip

--- lua/myconfig/plugins/nvim-spectre.lua
+++ lua/myconfig/plugins/nvim-spectre.lua
@@ -4,17 +4,27 @@ return {
         "nvim-lua/plenary.nvim",
     },
     config = function()
-        vim.keymap.set("n", "<leader>S", '<cmd>lua require("spectre").toggle()<CR>', {
+        vim.keymap.set("n", "<leader>sp", '<cmd>lua require("spectre").toggle()<CR>', {
             desc = "Spectre: toggle",
         })
-        vim.keymap.set("n", "<leader>sw", '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', {
-            desc = "Spectre: search current word",
-        })
+        vim.keymap.set(
+            "n",
+            "<leader>sw",
+            '<cmd>lua require("spectre").open_visual({select_word=true})<CR>',
+            {
+                desc = "Spectre: search current word",
+            }
+        )
         vim.keymap.set("v", "<leader>sw", '<esc><cmd>lua require("spectre").open_visual()<CR>', {
-            desc = "Spectre: search current word",
+            desc = "Spectre: search current selection",
         })
-        vim.keymap.set("n", "<leader>sp", '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', {
-            desc = "Spectre: search on current file",
-        })
+        vim.keymap.set(
+            "n",
+            "<leader>sP",
+            '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>',
+            {
+                desc = "Spectre: search on current file",
+            }
+        )
     end,
 }
