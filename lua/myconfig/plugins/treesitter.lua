311063d26cfbf44eee3c39e828225fb17b1b1537
remvnz <remvnz@gmail.com>
2025-12-15 13:01:12 +0000

    fix treesitter

--- lua/myconfig/plugins/treesitter.lua
+++ lua/myconfig/plugins/treesitter.lua
@@ -12,27 +12,48 @@ local languages = {
     "makefile",
     "sql",
 }
+local ignore_langs = require("lib.set").new()
 
 local treesitter = {
     "nvim-treesitter/nvim-treesitter",
     branch = "main",
     build = ":TSUpdate",
+    lazy = false,
     config = function()
-        require("nvim-treesitter").install(languages)
+        local ts = require("nvim-treesitter")
+        ts.install(languages)
 
         vim.api.nvim_create_autocmd("FileType", {
             pattern = { "*" },
             desc = "treesitter highlight autocmd",
             callback = function(args)
-                local buf, filetype = args.buf, args.match
-                local language = vim.treesitter.language.get_lang(filetype)
-                if not language or not vim.treesitter.language.add(language) then
+                local language = vim.treesitter.language.get_lang(args.match)
+                if not language or ignore_langs:has(language) then
                     return
                 end
 
-                vim.treesitter.start()
-                vim.wo[0][0].foldmethod = 'expr'
-                vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+                ---@param buf integer
+                ---@param lang string
+                local function start(buf, lang)
+                    vim.treesitter.start()
+                    vim.wo[0][0].foldmethod = "expr"
+                    vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
+                end
+
+                local installed_list = ts.get_installed()
+                if not vim.list_contains(installed_list, language) then
+                    local available_list = ts.get_available()
+                    if vim.list_contains(available_list, language) then
+                        ts.install(language):await(function()
+                            start(args.buf, language)
+                        end)
+                    else
+                        ignore_langs:add(language)
+                        return
+                    end
+                else
+                    start(args.buf, language)
+                end
             end,
         })
     end,
@@ -47,24 +68,9 @@ local textobjects = {
         vim.g.no_plugin_maps = true
     end,
     config = function()
-        require("nvim-treesitter-textobjects").setup {
-            -- prevent move and swap section add default mapping (?)
-            move = { enable = true },
-            swap = { enable = true },
-
+        require("nvim-treesitter-textobjects").setup({
             select = {
-                enable = true,
                 lookahead = true,
-                keymaps = {
-                    -- You can use the capture groups defined in textobjects.scm
-                    ["af"] = { query = "@function.outer", desc = "Select outside function" },
-                    ["if"] = { query = "@function.inner", desc = "Select inside function" },
-                    ["ac"] = { query = "@class.outer", desc = "Select around class" },
-                    ["ic"] = {
-                        query = "@class.inner",
-                        desc = "Select inner part of a class region",
-                    },
-                },
                 selection_modes = {
                     ["@parameter.outer"] = "v",
                     ["@function.outer"] = "V",
@@ -72,7 +78,32 @@ local textobjects = {
                 },
                 include_surrounding_whitespace = false,
             },
-        }
+        })
+
+        vim.keymap.set({ "x", "o" }, "af", function()
+            require("nvim-treesitter-textobjects.select").select_textobject(
+                "@function.outer",
+                "textobjects"
+            )
+        end, { desc = "function outer" })
+        vim.keymap.set({ "x", "o" }, "if", function()
+            require("nvim-treesitter-textobjects.select").select_textobject(
+                "@function.inner",
+                "textobjects"
+            )
+        end, { desc = "function innter" })
+        vim.keymap.set({ "x", "o" }, "ac", function()
+            require("nvim-treesitter-textobjects.select").select_textobject(
+                "@class.outer",
+                "textobjects"
+            )
+        end, { desc = "class outer" })
+        vim.keymap.set({ "x", "o" }, "ic", function()
+            require("nvim-treesitter-textobjects.select").select_textobject(
+                "@class.inner",
+                "textobjects"
+            )
+        end, { desc = "class inner" })
     end,
 }
 
--- lua/myconfig/plugins/ufo-fold.lua
+++ lua/myconfig/plugins/ufo-fold.lua
@@ -6,7 +6,7 @@ return {
     },
     config = function()
         vim.o.foldcolumn = "0" -- '0' is not bad
-        vim.o.foldlevel = 99   -- Using ufo provider need a large value, feel free to decrease the value
+        vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
         vim.o.foldlevelstart = 99
         vim.o.foldenable = true
 
@@ -42,7 +42,7 @@ return {
         require("ufo").setup({
             fold_virt_text_handler = handler,
             provider_selector = function(bufnr, filetype, buftype)
-                return { "indent" }
+                return { "treesitter", "indent" }
             end,
         })
     end,
