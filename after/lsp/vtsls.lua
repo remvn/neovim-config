1781ec13715a0ba3214ca8a367ff49eff2ce5463
remvnz <remvnz@gmail.com>
2026-04-09 10:40:11 +0000

    add vtsls organize imports command

--- after/lsp/vtsls.lua
+++ after/lsp/vtsls.lua
@@ -1,12 +1,24 @@
-local vue_language_server_path = vim.fn.expand '$MASON/packages' ..
-    '/vue-language-server' .. '/node_modules/@vue/language-server'
+---@diagnostic disable: missing-fields
+local vue_language_server_path = vim.fn.expand("$MASON/packages")
+    .. "/vue-language-server"
+    .. "/node_modules/@vue/language-server"
 local vue_plugin = {
-    name = '@vue/typescript-plugin',
+    name = "@vue/typescript-plugin",
     location = vue_language_server_path,
-    languages = { 'vue' },
-    configNamespace = 'typescript',
+    languages = { "vue" },
+    configNamespace = "typescript",
 }
 
+local function organize_imports()
+    vim.lsp.buf.code_action({
+        apply = true,
+        context = {
+            only = { "source.organizeImports.ts", "source.organizeImports" },
+        },
+    })
+end
+
+---@type vim.lsp.ClientConfig
 return {
     settings = {
         vtsls = {
@@ -17,5 +29,16 @@ return {
             },
         },
     },
-    filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
+    filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+    on_attach = function(_, bufnr)
+        vim.keymap.set(
+            "n",
+            "<leader>oi",
+            organize_imports,
+            { buffer = bufnr, desc = "Organize Imports" }
+        )
+        vim.api.nvim_buf_create_user_command(bufnr, "OrganizeImports", organize_imports, {
+            desc = "Organize Imports",
+        })
+    end,
 }
